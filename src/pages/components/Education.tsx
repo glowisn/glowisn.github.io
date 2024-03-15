daffbc9f8f6d813dd68cfe592bb393123ca07890
Yechan Jung <asdxcv1998@gmail.com>
2024-03-15 23:39:40 +0900

    Update Education component

--- src/pages/components/Education.tsx
+++ src/pages/components/Education.tsx
@@ -2,7 +2,17 @@
 export default function Education() {
   return (
     <>
-     <h2 className="text-2xl font-bold">Education</h2>
+     <h2 className="text-3xl font-bold mb-[0.5em]">Education</h2>
+     <ul>
+        <li className="my-[0.5em]">
+          <h3 className="text-xl font-bold">네이버 부스트캠프 웹모바일 8기 멤버쉽 수료</h3>
+          <p>2023.07 ~ 2023.12</p>
+        </li>
+        <li className="my-[0.5em]">
+          <h3 className="text-xl font-bold">한동대학교 AI·컴퓨터공학심화 졸업</h3>
+          <p>2017.03 ~ 2023.08</p>
+        </li>
+     </ul>
     </>
   );
 }
